9d1132dc9e79a6a3e353df8303365b6589a51532
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-04 12:51:46 +0800

    Create DOORS-UnusedContent-pt1.lua
    idk

---
+++ DOORS-UnusedContent-pt1.lua
@@ -0,0 +1,6 @@
+--Pumpkin head
+game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.PumpkinVignette.Visible = true
+--Fire
+game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Healthbar.Effects.Fire.Visible = true
+--InFiredamp
+game.Players.LocalPlayer.PlayerGui.MainUI.MainFrame.Healthbar.Effects.InFiredamp.Visible = true
