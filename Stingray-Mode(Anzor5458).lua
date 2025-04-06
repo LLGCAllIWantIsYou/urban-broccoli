583d063a617b0af2273772238b557a38c2281948
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-06 11:16:32 +0800

    Create Stingray-Mode(Anzor5458).lua

---
+++ Stingray-Mode(Anzor5458).lua
@@ -0,0 +1,34 @@
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Made by anzor and chatGPT",true)
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+wait(1)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("stingray mode,Details by FOCUS,LLGC and ASH_Uranium235",true)
+wait(3)
+
+-- Smiley 
+coroutine.wrap(function()
+    while true do
+        wait(math.random(50,301))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(9)
+loadstring(game:HttpGet('https://pastebin.com/raw/R5i67c3H'))()
+    end
+end)()
+
+-- Pipsqueak
+coroutine.wrap(function()
+    while true do
+        wait(math.random(37,120))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(4)
+loadstring(game:HttpGet('https://pastebin.com/raw/epkKZ3ak'))()
+    end
+end)()
+-- Abyss 
+coroutine.wrap(function()
+    while true do
+        wait(320)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(2)
+loadstring(game:HttpGet('https://pastebin.com/raw/UsJxmdnf'))()
+    end
+end)()
