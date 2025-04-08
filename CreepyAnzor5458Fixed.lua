5dfbe0a1c0d80453cac7b8fb0ec9c998e5b7cc92
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-09 02:01:04 +0800

    Update CreepyAnzor5458Fixed.lua

--- CreepyAnzor5458Fixed.lua
+++ CreepyAnzor5458Fixed.lua
@@ -15,3 +15,21 @@ end)
 if not success then
     warn("Failed to load A1 script: " .. tostring(result))
 end
+
+--D1
+local B1Script = game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/door.lua")
+local success, result = pcall(function()
+    loadstring(B1Script)()
+end)
+if not success then
+    warn("Failed to load A1 script: " .. tostring(result))
+end
+
+--D1
+local C1Script = game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Fog.lua")
+local success, result = pcall(function()
+    loadstring(C1Script)()
+end)
+if not success then
+    warn("Failed to load A1 script: " .. tostring(result))
+end
