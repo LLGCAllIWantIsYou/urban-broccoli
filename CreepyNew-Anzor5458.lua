275561a13724c801c3bf98bb1b06dc080d85c8fd
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-09 00:31:45 +0800

    Create CreepyNew-Anzor5458.lua

---
+++ CreepyNew-Anzor5458.lua
@@ -0,0 +1,34 @@
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Made by Anzor5458.",true)
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+wait(1)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Creepy Mode,Fixed by FOCUS,LLGC and ASH_Uranium235.",true)
+wait(3)
+
+-- Chainsomker
+coroutine.wrap(function()
+    while true do
+        wait(math.random(50,400))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(9)
+loadstring(game:HttpGet('https://pastebin.com/raw/4447cfUc'))()
+    end
+end)()
+
+-- Froger
+coroutine.wrap(function()
+    while true do
+        wait(math.random(70,360))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(4)
+loadstring(game:HttpGet('https://pastebin.com/raw/8RukAVE3'))()
+    end
+end)()
+--200MovingGlitch
+coroutine.wrap(function()
+    while true do
+        wait(520)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(2)
+loadstring(game:HttpGet('https://pastebin.com/raw/QZM7mnWJ'))()
+    end
+end)()
