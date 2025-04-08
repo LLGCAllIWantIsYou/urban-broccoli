975a5f239d8d0df2edb3ef7a62db29be7b4c1c6b
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-09 00:51:44 +0800

    Create CreepyAnzor5458Fixed.lua

---
+++ CreepyAnzor5458Fixed.lua
@@ -0,0 +1,44 @@
+--Main
+local kainScript = game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/CreepyNew-Anzor5458.lua")
+local success, result = pcall(function()
+    loadstring(kainScript)()
+end)
+if not success then
+    warn("Failed to load kain script: " .. tostring(result))
+end
+
+--D1
+local A1Script = game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/idk.lua")
+local success, result = pcall(function()
+    loadstring(A1Script)()
+end)
+if not success then
+    warn("Failed to load A1 script: " .. tostring(result))
+end
+
+--D2
+local A2Script = game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/Other/Fake%20RTX.lua")
+local success, result = pcall(function()
+    loadstring(A2Script)()
+end)
+if not success then
+    warn("Failed to load A2 script: " .. tostring(result))
+end
+
+--D3
+local A3Script = game:HttpGet("https://pastebin.com/raw/6brG9uma")
+local success, result = pcall(function()
+    loadstring(A3Script)()
+end)
+if not success then
+    warn("Failed to load A3 script: " .. tostring(result))
+end
+
+-- D4
+local A4Script = game:HttpGet("https://pastebin.com/raw/iAhTGdBh")
+local success, result = pcall(function()
+    loadstring(A4Script)()
+end)
+if not success then
+    warn("Failed to load A4 script: " .. tostring(result))
+end
