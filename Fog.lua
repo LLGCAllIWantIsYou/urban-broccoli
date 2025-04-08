919380198d8bbd1ede6cfb06007ec0c80dbbf306
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-09 01:57:24 +0800

    Create Fog.lua

---
+++ Fog.lua
@@ -0,0 +1,10 @@
+-- Load the Fog script
+local Fog = loadstring(game:HttpGet("https://pastebin.com/raw/HnD10RRW"))()
+Fog({
+    enableAtmosphere = true,
+    ambientColor = Color3.new(1, 1, 1),
+    brightnessValue = 0.5,
+    fogStartValue = 100,
+    fogEndValue = 200,
+    fogColor = Color3.new(1, 1, 1)
+})
