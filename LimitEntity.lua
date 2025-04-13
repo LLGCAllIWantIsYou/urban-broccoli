1bf1dc6561dc223cea2e1eda3ef9e17fda2e2834
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-13 10:08:51 +0800

    Create LimitEntity.lua

---
+++ LimitEntity.lua
@@ -0,0 +1,28 @@
+-- Screamer
+coroutine.wrap(function()
+    while true do
+        wait(math.random(50,201))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(9)
+loadstring(game:HttpGet('https://pastebin.com/raw/6FxdfrcK'))()
+    end
+end)()
+
+-- depth
+coroutine.wrap(function()
+    while true do
+        wait(math.random(37,220))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(4)
+loadstring(game:HttpGet('https://pastebin.com/raw/kWeCbvQm'))()
+    end
+end)()
+-- monoxide
+coroutine.wrap(function()
+    while true do
+        wait(420)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(2)
+loadstring(game:HttpGet('https://pastebin.com/raw/CtvvNBY7'))()
+    end
+end)()
