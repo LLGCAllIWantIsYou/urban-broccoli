55d1ea1f9da33fd4c69c92be7c524145a3f428b1
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-04-21 22:55:29 +0800

    Create Light.lua

---
+++ Light.lua
@@ -0,0 +1,4 @@
+local lighting = game:GetService("Lighting")
+lighting.Brightness = 1
+lighting.ColorShift_Top = Color3.fromRGB(0, 170, 255)
+lighting.ColorShift_Bottom = Color3.fromRGB(0, 170, 255)
