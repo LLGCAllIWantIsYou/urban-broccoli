33ce70eb03c0e7491804a4db1c86a60cb9ea9d33
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-05-11 22:14:00 +0800

    Create AchievementExample.lua

---
+++ AchievementExample.lua
@@ -0,0 +1,10 @@
+--Depth
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "It's Cold Here...",
+    Desc = "In That Blue Mouth.",
+    Reason = "Killed By Depth.",
+    Image = "rbxassetid://17651731518"
+})
