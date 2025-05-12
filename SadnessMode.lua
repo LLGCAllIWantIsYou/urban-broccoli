c6831a7fac9f0ae307571b43a616d7ea3c4e3872
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-05-13 01:23:01 +0800

    Update SadnessMode.lua

--- SadnessMode.lua
+++ SadnessMode.lua
@@ -1,3 +1,14 @@
+--Depth
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "It's Cold Here...",
+    Desc = "A melancholic world.",
+    Reason = "February 29, 2000.",
+    Image = "rbxassetid://17651731518"
+})
+--
 --I am so sad
 --Hate me
 --Sorrow
