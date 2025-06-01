6bbcebca7ba88bc561b01dbcca60746a57c802bd
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-06-01 18:43:18 +0800

    Create Math.lua

---
+++ Math.lua
@@ -0,0 +1,34 @@
+print(math.abs(-5)) -- 5
+print(math.acos(0.5)) -- 1.0471975511966
+print(math.asin(0.5)) -- 0.5235987755983
+print(math.atan(1)) -- 0.7853981633974
+print(math.atan2(1, 1)) -- 0.7853981633974
+print(math.ceil(3.2)) -- 4
+print(math.cos(0)) -- 1
+print(math.cosh(1)) -- 1.5430806348152
+print(math.deg(math.pi)) -- 180
+print(math.exp(1)) -- 2.7182818284591
+print(math.floor(3.8)) -- 3
+print(math.fmod(10, 3)) -- 1
+print(math.frexp(10)) -- 0.625, 4
+print(math.huge) -- inf
+print(math.ldexp(0.625, 4)) -- 10
+print(math.log(2.7182818284591)) -- 1
+print(math.log10(100)) -- 2
+print(math.max(1, 2, 3)) -- 3
+print(math.min(1, 2, 3)) -- 1
+print(math.modf(3.7)) -- 3, 0.7
+print(math.pi) -- 3.1415926535898
+print(math.pow(2, 3)) -- 8
+print(math.rad(180)) -- 3.1415926535898
+print(math.random()) -- a random number between 0 and 1
+math.randomseed(12345) -- sets the seed for the random number generator
+print(math.sin(0)) -- 0
+print(math.sinh(1)) -- 1.1752011936438
+print(math.sqrt(16)) -- 4
+print(math.tan(0)) -- 0
+print(math.tanh(1)) -- 0.7615941559558 print(math.type(3.14)) -- "float"print(math.type(3)) -- "integer"
+print(math.tointeger(3.14)) -- nil
+print(math.tointeger(3)) -- 3
+print(math.ult(3, 5)) -- true
+print(math.ult(5, 3)) -- false
