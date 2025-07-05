02ccae1e15041788c7f35bbf170e26e2e2487e7f
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-07-05 14:17:31 +0800

    Update Light.lua

--- Light.lua
+++ Light.lua
@@ -1,4 +1,4 @@
 local lighting = game:GetService("Lighting")
-lighting.Brightness = 1
-lighting.ColorShift_Top = Color3.fromRGB(0, 170, 255)
-lighting.ColorShift_Bottom = Color3.fromRGB(0, 170, 255)
+lighting.Brightness = 0.5
+lighting.ColorShift_Top = Color3.fromRGB(255, 0, 0)
+lighting.ColorShift_Bottom = Color3.fromRGB(255, 0, 0)
