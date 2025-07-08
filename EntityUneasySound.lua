5636e13c49938eba20dc35fa078e0f337d535ca5
LLGCAllIWantIsYou <deergod98@gmail.com>
2025-07-08 09:50:06 +0800

    Create EntityUneasySound.lua

---
+++ EntityUneasySound.lua
@@ -0,0 +1,15 @@
+-- Uneasy screech
+game.ReplicatedStorage.Entities.Screech.Top.Eyes.Color = Color3.fromRGB(255, 170, 255)
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech.Caught.SoundId = "rbxassetid://6065824739"
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech.Caught.PlaybackSpeed = 0.8
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.Screech.Attack.SoundId = "rbxassetid://89279317360150"
+
+-- Uneasy hide
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.HideMonster.Scare.SoundId = "rbxassetid://107508830539142"
+
+-- Uneasy Timothy
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.RemoteListener.Modules.SpiderJumpscare.Scare.SoundId = "rbxassetid://7081093332"
+
+--Uneasy Darkness
+game.Workspace.Ambience_Dark.SoundId = "rbxassetid://5521959695"
+game.Workspace.Ambience_Dark.PlaybackSpeed = 1.1
